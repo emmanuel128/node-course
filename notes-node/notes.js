c26d99a535e24da1c74b9aa662bb906357ef826e
Emmanuel D. Castro Soto <ecastro@invidgroup.com>
2018-06-07 17:36:41 -0400

    debug

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -48,6 +48,8 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
+    debugger;
+    console.log(`----`)
     console.log(`Title: ${note.title}`)
     console.log(`Body: ${note.body}`)
 };
---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Emma'
+};
+
+person.age = 25;
+
+debugger;
+
+person.name = 'Ale';
+
+console.log(person);
