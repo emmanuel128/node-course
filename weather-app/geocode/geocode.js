fc59f8daa4be3b4ea9504954561e22fd2a52c393
Emmanuel D. Castro Soto <ecastro@invidgroup.com>
2018-06-08 15:46:22 -0400

    abstracting callbacks

---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+geocodeAddress = (address, callback) => {
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyCI_R3wN76sy72ZatATUzRQbPHbPFUOTXw`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            // console.log('Error:', error);
+            callback('Unable to connect to Google servers.')
+        } else if (body.status === 'ZERO_RESULTS') {
+            // console.log('Unable to find that address.');
+            callback('Unable to find that address.');
+        }
+        else if (body.status === 'OK') {
+            // console.log(JSON.stringify(body, null, 2))
+            // console.log(`Address: ${body.results[0].formatted_address}`);
+            // console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            // console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+            callback(null, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            })
+        }
+    })
+}
+
+module.exports = {
+    geocodeAddress
+}
