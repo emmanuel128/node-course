89d8024a20ec7f06f1f0cd36bb9b1144e3388b8a
emmanuel128 <emmanuel.castro@upr.edu>
2018-07-07 15:47:45 -0400

    mocha descripbe implemented

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,30 +2,36 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
 
-    expect(res).toBe(44).toBeA('number');
-});
+            expect(res).toBe(44).toBeA('number');
+        });
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
-    });
-});
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
+    })
 
-it('should square a number', () => {
-    var res = utils.square(3);
+    describe('#square', () => {
+        it('should square a number', () => {
+            var res = utils.square(3);
 
-    expect(res).toBe(9).toBeA('number');
-});
+            expect(res).toBe(9).toBeA('number');
+        });
 
-it('should async square a number', (done) => {
-    utils.asyncSquare(3, (square) => {
-        expect(square).toBe(9).toBeA('number');
-        done();
-    });
+        it('should async square a number', (done) => {
+            utils.asyncSquare(3, (square) => {
+                expect(square).toBe(9).toBeA('number');
+                done();
+            });
+        });
+    })
 });
 
 it('should expect some values', () => {
