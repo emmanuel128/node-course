eaae8937667316269fce4fc795826d09722adf56
emmanuel128 <emmanuel.castro@upr.edu>
2018-07-07 22:55:53 -0400

    add validations

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,34 +5,76 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-// var newTodo = new Todo({
-//     text: 'Cook dinner',
+// var otherTodo = new Todo({
+//     text: 'Other Todo',
+//     completed: true,
 // });
 
-// newTodo.save().then((doc) => {
+// otherTodo.save().then((doc) => {
 //     console.log('Saved todo.', doc);
+//     disconnect();
 // }, (error) => {
 //     console.log('Unable to save todo.', error);
+//     disconnect();
 // });
 
-var otherTodo = new Todo({
-    text: 'Other Todo',
-    completed: true,
-    completedAt: 123
+// User
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    },
+    location: {
+        type: String,
+        default: null
+    },
+    age: {
+        type: Number,
+        required: true
+    }
 });
 
-otherTodo.save().then((doc) => {
-    console.log('Saved todo.', doc);
-}, (error) => {
-    console.log('Unable to save todo.', error);
+var newUser = new User({
+    email: 'email@domain.com',
+    name: 'Emmanuel',
+    age: 24
 });
+
+newUser.save().then((doc) => {
+    console.log('Saved user.', doc);
+    disconnect();
+}, (error) => {
+    console.log('Unable to save user.', error);
+    disconnect();
+});
+
+function disconnect() {
+    mongoose.disconnect().then(() => {
+        console.log('closed');
+    }, error => {
+        console.log('error', error);
+    });
+}
