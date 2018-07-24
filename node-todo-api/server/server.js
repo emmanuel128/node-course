6ff3c0f0265e2c753e66a7575524c018f26ca4b4
lruiz-invidgroup <lruiz@invidgroup.com>
2018-07-24 15:41:36 -0400

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb')
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -34,6 +34,30 @@ app.get('/todos', (req, res) => {
     }).catch((err) => {
         res.status(500).send(err);
     });
+});
+
+// GET /toods/123
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({ todo });
+        }).catch((e) => {
+            res.status(400).send();
+        })
+    // findById
+    // success
+    // error
+    // 400 - 
 });
 
 app.listen(PORT, () => {
